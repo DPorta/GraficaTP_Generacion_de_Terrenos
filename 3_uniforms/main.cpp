4bf571e25966969252b946fb4074da7168ddf6f3
AldoGomezz <aldojhairgomez@gmail.com>
2021-06-04 01:00:31 -0500

    Camera

--- 3_uniforms/main.cpp
+++ 3_uniforms/main.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include "Cube.h"
+#include "Camera.h"
 
 const ui32 FSIZE = sizeof(f32);
 const ui32 ISIZE = sizeof(i32);
@@ -11,115 +12,80 @@ const ui32 SCR_WIDTH = 1920;
 const ui32 SCR_HEIGHT = 1080;
 const f32  ASPECT = (f32)SCR_WIDTH / (f32)SCR_HEIGHT;
 
-//valores para la camara
-glm::vec3 position = glm::vec3(25.0f, 10.0f, 50.0f);
-glm::vec3 front = glm::vec3(25.0f, -10.0f, 0.0f);
-glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-
-//para el mouse
-bool firstMouse = true;
-f32 yaw = -90.0f;
-f32 pitch = 0.0f;
-f32 lastX = SCR_WIDTH / 2.0f;
-f32 lastY = SCR_HEIGHT / 2.0f;
-
-//para el scroll (zoom)
-f32 fov = 45.0f;
-
-//para el tiempo y que el programa se vea mas suave
-f32 deltaTime = 0.0f;
-f32 lastFrame = 0.0f;
 
 //PERLIN NOISE TERRENO
 ui32 y_level = 7;
-const ui32 x_max{ 50 };
-const ui32 z_max{ 50 };
+const ui32 x_max{ 10 };
+const ui32 z_max{ 10 };
 double gradient[x_max][z_max][3];
 #include "PerlinNoiseTerrain.h"
 double terreno[x_max][z_max];
 std::vector<glm::vec4> transition;
 
-//procesar teclas
+Cam* cam = new Cam();
+f32  lastx;
+f32  lasty;
+bool firstMouse = true;
+f32  deltaTime = 0.0f;
+f32  lastFrame = 0.0f;
+/**
+ * keyboard input processing
+ **/
 void processInput(GLFWwindow* window) {
-
-
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
-	f32 speed = 2.5 * deltaTime;
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-		position += speed * front;
+		cam->processKeyboard(FORWARD, deltaTime);
 	}
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-		position -= glm::normalize(glm::cross(front, up)) * speed;
+		cam->processKeyboard(LEFT, deltaTime);
 	}
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-		position -= speed * front;
+		cam->processKeyboard(BACKWARD, deltaTime);
 	}
 	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-		position += glm::normalize(glm::cross(front, up)) * speed;
+		cam->processKeyboard(RIGHT, deltaTime);
 	}
-
 }
 
-//procesar mouse
-void mouseCallBack(GLFWwindow*window, f64 xpos, f64 ypos) {
-	if (firstMouse) {
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
+void mouse_callback(GLFWwindow* window, f64 xpos, f64 ypos) {
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		if (firstMouse) {
+			lastx = xpos;
+			lasty = ypos;
+			firstMouse = false;
+			return;
+		}
+		cam->processMouse((f32)(xpos - lastx), (f32)(lasty - ypos));
+		lastx = xpos;
+		lasty = ypos;
 	}
-	f32 xoffset = xpos - lastX;
-	f32 yoffset = lastY - ypos;
-
-	lastX = xpos;
-	lastY = ypos;
-
-	f32 sensitivity = 0.05f;
-	xoffset *= sensitivity;
-	yoffset *= sensitivity;
-
-	yaw += xoffset;
-	pitch += yoffset;
-
-	if (pitch > 89.0f) {
-		pitch = 89.0f;
+	else {
+		firstMouse = true;
 	}
-	else if (pitch <-89.0f) {
-		pitch = -89.0f;
-	}
-	glm::vec3 f;
-	f.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-	f.y = sin(glm::radians(pitch));
-	f.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-	front = glm::normalize(f);
 }
 
-//procesar scroll
-void scrollCallBack(GLFWwindow* , f64 xoffset, f64 yoffset) {
-	fov -= (f32)yoffset; //para el zoom
-	if (fov < 1.0f) {
-		fov = 1.0f;
-	}
-	else if (fov > 45.0f) {
-		fov = 45.0f;
-	}
-
+void scroll_callback(GLFWwindow* window, f64 xoffset, f64 yoffset) {
+	cam->processScroll((f32)yoffset);
 }
-
 
 i32 main() {
 
 	GLFWwindow* window = glutilInit(3, 3, SCR_WIDTH, SCR_HEIGHT, "Terreno: Campo");
+
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 	Shader* shader = new Shader();
+	
+	
+	cam = new Cam();
 	Cube* cubex = new Cube();
 	MyTerrain* miTerreno = new MyTerrain();
-
+	
 	miTerreno->genTerrain<x_max, z_max>(terreno);
 	miTerreno->genTerrainTransitions<x_max, z_max>(terreno, transition, y_level);
 
-	glfwSetCursorPosCallback(window, mouseCallBack);
-	glfwSetScrollCallback(window, scrollCallBack);
 		
 	//VBO, VAO, EBO
 	ui32 vbo, vao, ebo;
@@ -158,6 +124,7 @@ i32 main() {
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
+
 		processInput(window);
 		glClearColor(0.98f, 0.45f, 0.0f, 0.5f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -169,12 +136,11 @@ i32 main() {
 		shader->useProgram();
 		
 		//MATRICES PARA LA CAMARA
-		glm::mat4 projection = glm::perspective(glm::radians(fov), ASPECT, 0.1f, 100.0f);
+		glm::mat4 projection = glm::perspective(cam->getZoom(), ASPECT, 0.1f, 100.0f);
 		shader->setMat4("proj", projection);
 
 		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::lookAt(position,  position + front, up);
-		shader->setMat4("view", view);
+		shader->setMat4("view", cam->getViewM4());
 
 		glBindVertexArray(vao);
 
